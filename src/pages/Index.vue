4a5fdc9aa066445c4ebce2dfd7c34f5c97a031e3
Robert Axelsen <rob@rob.ee>
2019-05-02 20:45:30 +0200

    Add top-most content additional images and talks section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,9 +3,9 @@
     <div class="creator container-inner mx-auto flex justify-between py-16">
       <div class="text-4xl font-bold w-3/5">
         <div>
-          I'm all about Code
+          I'm all about Web,
           <br />
-          Web & Community...
+          Code and People...
         </div>
         <div class="text-green">
           A pleasure to meet you!
@@ -17,6 +17,24 @@
     </div>
     <div class="container-inner mx-auto">
       Hello world! I am Robert, a software developer at <a href="https://www.sipwise.com/">Sipwise GmbH</a>, meetup organizer, founder of <a href="http://www.developersrising.com/">Developers Rising</a>, speaker and creator with a passion for sharing my love of code with other people. Oh, and I love learning. The meetups I organize are called <a href="https://www.meetup.com/Free-Code-Camp-Vienna/">Free Code Camp Vienna (Austria)</a>, <a href="https://www.meetup.com/Vue-js-Vienna/">Vue.js Vienna</a> and <a href="https://www.meetup.com/vienna-go-Vienna-Go-User-Group/">vienna.go – Vienna Go User Group</a> . All are monthly meetups where everyone is free to join, no matter experience level. I am always open to connect, hear about opportunities, and learn how to help people even more. Please connect with me below. Until then, ROCK ON!
+    </div>
+    <div class="container-inner mx-auto flex justify-between py-16">
+      <div>
+        <g-image alt="Creator standing by wall with painting" src="../../static/code.svg" width="285" />
+      </div>
+      <div>
+        <g-image alt="Creator standing by wall with painting" src="../../static/celebration.svg" width="285" />
+      </div>
+    </div>
+
+    <div class="talks container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 ">
+      <h2 class="font-bold mb-6">Here are some talks I've been giving at conferences and meetups:</h2>
+        <ul>
+          <li class="checkmark mb-6">Talk One. Lorem ipsum dolor, sit amet consectetur sdipisicing elit.</li>
+          <li class="checkmark mb-6">Talk Two. Lorem ipsum dolor, sit amet consectetur sdipisicing elit.</li>
+          <li class="checkmark mb-6">Talk Three. Lorem ipsum dolor, sit amet consectetur sdipisicing elit.</li>
+          <li class="checkmark mb-6">Talk Four. Lorem ipsum dolor, sit amet consectetur sdipisicing elit.</li>
+        </ul>
     </div>
   </Layout>
 </template>
