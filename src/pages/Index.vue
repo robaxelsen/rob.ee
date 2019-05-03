592ff0bf575a28e36c06a2ad01ed6984cc24003b
Robert Axelsen <rob@rob.ee>
2019-05-03 20:46:49 +0200

    Change text formatting, replace top-most graphic with photo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,28 +12,34 @@
         </div>
       </div>
       <div>
-        <g-image alt="Creator standing by wall with painting" src="../../static/programmer.svg" width="285" />
+        <g-image alt="Rob Axelsen standing on stage moderating a panel" src="../../static/rob_on_stage.png" width="285" />
       </div>
     </div>
-    <div class="container-inner mx-auto">
-      Hello world! I am Robert, a software developer at <a href="https://www.sipwise.com/">Sipwise GmbH</a>, meetup organizer, founder of <a href="http://www.developersrising.com/">Developers Rising</a>, speaker and creator with a passion for sharing my love of code with other people. Oh, and I love learning. The meetups I organize are called <a href="https://www.meetup.com/Free-Code-Camp-Vienna/">Free Code Camp Vienna (Austria)</a>, <a href="https://www.meetup.com/Vue-js-Vienna/">Vue.js Vienna</a> and <a href="https://www.meetup.com/vienna-go-Vienna-Go-User-Group/">vienna.go – Vienna Go User Group</a> . All are monthly meetups where everyone is free to join, no matter experience level. I am always open to connect, hear about opportunities, and learn how to help people even more. Please connect with me below. Until then, ROCK ON!
+    <div class="container-inner mx-auto text-xl mb-6">
+      Hello world! I am Robert, a software developer at <a href="https://www.sipwise.com/">Sipwise GmbH</a>, meetup organizer, founder of <a href="http://www.developersrising.com/">Developers Rising</a>, speaker and creator with a passion for sharing my love of code with other people.
+    </div> 
+    <div class="container-inner mx-auto text-xl mb-6">
+      The meetups I organize are called <a href="https://www.meetup.com/Free-Code-Camp-Vienna/">Free Code Camp Vienna (Austria)</a>, <a href="https://www.meetup.com/Vue-js-Vienna/">Vue.js Vienna</a> and <a href="https://www.meetup.com/vienna-go-Vienna-Go-User-Group/">vienna.go – Vienna Go User Group</a> . All are monthly meetups where everyone is free to join, no matter experience level.
+    </div>
+    <div class="container-inner mx-auto text-xl mb-6">
+      I am always open to connect, learn new things, and for opportunities to help. Please connect with me below. Until then, ROCK ON!
     </div>
     <div class="container-inner mx-auto flex justify-between py-16">
       <div>
-        <g-image alt="Creator standing by wall with painting" src="../../static/code.svg" width="285" />
+        <g-image alt="Programmer looking at git branch tree" src="../../static/programmer.svg" width="285" />
       </div>
       <div>
-        <g-image alt="Creator standing by wall with painting" src="../../static/celebration.svg" width="285" />
+        <g-image alt="People celebration open source community success" src="../../static/celebration.svg" width="285" />
       </div>
     </div>
 
     <div class="talks container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 ">
       <h2 class="font-bold mb-6">Here are some talks I've been giving at conferences and meetups:</h2>
         <ul>
-          <li class="checkmark mb-6">Talk One. Lorem ipsum dolor, sit amet consectetur sdipisicing elit.</li>
-          <li class="checkmark mb-6">Talk Two. Lorem ipsum dolor, sit amet consectetur sdipisicing elit.</li>
-          <li class="checkmark mb-6">Talk Three. Lorem ipsum dolor, sit amet consectetur sdipisicing elit.</li>
-          <li class="checkmark mb-6">Talk Four. Lorem ipsum dolor, sit amet consectetur sdipisicing elit.</li>
+          <li class="checkmark mb-6">Talk One. Lorem ipsum dolor, sit amet consectetur dipisicing elit.</li>
+          <li class="checkmark mb-6">Talk Two. Lorem ipsum dolor, sit amet consectetur dipisicing elit.</li>
+          <li class="checkmark mb-6">Talk Three. Lorem ipsum dolor, sit amet consectetur dipisicing elit.</li>
+          <li class="checkmark mb-6">Talk Four. Lorem ipsum dolor, sit amet consectetur dipisicing elit.</li>
         </ul>
     </div>
   </Layout>
@@ -42,7 +48,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Web, Code and People'
   }
 }
 </script>
